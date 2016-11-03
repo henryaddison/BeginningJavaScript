d2453bc0e8d6c183c760e0aa642c6ada032f99ad
Henry Addison <hja22@hja22.co.uk>
2016-11-03 22:19:12 +0000

    add poker simulation

--- 09-poker-simulation/simulation.js
+++ 09-poker-simulation/simulation.js
@@ -1,3 +1,36 @@
 // simulation goes here
 
 console.log("poker simulation");
+
+var deck = suits.reduce(function(protoDeck, suit) {
+  var suitDeckPart = ranks.map(function(rank) {
+    return {"suit": suit, "rank": rank};
+  });
+  protoDeck = protoDeck.concat(suitDeckPart);
+  return protoDeck;
+}, []);
+
+if(!isDeck(deck)) {
+  throw "do not have a properly formed deck";
+}
+
+var simulatePoker = function(runs, deck) {
+  var counts = {};
+  for(var i=0;i<runs;i=i+1) {
+    var shuffledDeck = shuffle(deck);
+    var hand = dealHand(shuffledDeck);
+
+    if(!isHand(hand)) {
+      throw "do not have a properly formed deck";
+    }
+
+    var handType = highestRank(hand);
+    if(!counts[handType]) {
+      counts[handType] = 0;
+    }
+    counts[handType] = counts[handType] + 1;
+  }
+  return counts
+}
+
+console.log(simulatePoker(1000, deck));
